8d593ffc293f26718938b690df21b4840f5af842
fabiano3319 <fabianoramefiarison@gmail.com>
2026-09-17 18:38:31 +0300

    feat: ajout du contenu dans models.py, views.py et serializers.py

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20)
+    email = models.EmailField(blank=True)
+    categorie = models.CharField(max_length=50, blank=True)  # ex: famille, travail, ami
+    date_ajout = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
